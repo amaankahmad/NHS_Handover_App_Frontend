4e33669c72b3aa19f0e83d12872b8116bcda7327
nathanmawer <nathanmawer99@outlook.com>
2021-12-15 15:13:30 +0000

    Forgot Password Screen Update

---
+++ nhs_handover/src/Components/Button2.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button2 = styled.button`
+    width: 400px;
+    height: 100px;
+    background-color: white;
+    margin: 6.4em 25em 4em;
+    font-size: 1.5em;
+    text-align: right;
+    color: #21ACFA;
+    border: none;
+
+
+`  
+;
---
+++ nhs_handover/src/Components/Container2.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container2 = styled.div`
+    width: 679px;
+    height: 159px;
+    background-color: #21ACFA;
+    margin: 3em 6em; 
+    display: inline-block;
+    text-align: center;
+
+`  
+;
