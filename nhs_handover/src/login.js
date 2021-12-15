e2cf49f53f52094b679f5774c8e2c137caf72f5b
is419 <is419@ic.ac.uk>
2021-12-15 15:43:00 +0000

    v2

--- nhs_handover/src/login.js
+++ nhs_handover/src/login.js
@@ -11,15 +11,16 @@ function Header(props){
     return(
 
         <header>
-            <img src={handover_logo} height={80} alt="logo"/>
-            <img src={imperial_logo} height={50} alt="logo"/>
-            <h1 style={{color:'lightBlue'}}>Login Page</h1>
+            <img src={handover_logo} id="ourLogo" height={80} alt="logo"/>
+            <img src={imperial_logo} id="nhsLogo" height={50} alt="logo"/>
+            <h1>Login Page</h1>
         </header>
 
     )
 }
 
 function Main(){
+
     const [data,setData]=useState(null)
     function getData(val)
     {
@@ -27,13 +28,17 @@ function Main(){
         console.warn(val.target.value)
     }
     return(
-        <section style={{backgroundColor:'lightblue'}}>
+        <div id = "mainBox">
+
+            {/*<section style={{backgroundColor:'lightskyblue'}}>*/}
 
             <p>NHS ID: <input type="text" onChange={getData}/></p>
             <p>Password: <input type="text"/></p>
 
-        </section>
+        {/*</section>*/}
+    </div>
     )
+
 }
 function Footer(){
     let navigate = useNavigate();
@@ -44,14 +49,14 @@ function Footer(){
     return(
 
         <footer>
-            <button onClick={handleClick} style={{backgroundColor:'lightblue'}}>
+            <button id="button1" onClick={handleClick} style={{backgroundColor:'lightskyblue'}}>
                 Login
             </button>
-            <button style={{backgroundColor:'lightblue'}}>
+            <button id="button2" style={{backgroundColor:'lightskyblue'}}>
                 Forgot Password
             </button>
             <p></p>
-            <button style={{backgroundColor:'lightblue'}}>
+            <button id="button3" style={{backgroundColor:'lightskyblue'}}>
                 Sign up
             </button>
         </footer>
@@ -61,8 +66,8 @@ function Header2(){
     return(
 
         <header>
-            <img src={handover_logo} height={80} alt="logo"/>
-            <img src={imperial_logo} height={50} alt="logo"/>
+            <img src={handover_logo} id="ourLogo" height={80} alt="logo"/>
+            <img src={imperial_logo} id="nhsLogo" height={50} alt="logo"/>
         </header>
 
     )
@@ -74,7 +79,7 @@ function Main2(){
         <section>
 <p></p>
             <div>
-            <button style={{backgroundColor:'lightblue'}}>
+            <button id="buttonSun" style={{backgroundColor:'lightskyblue'}}>
                 <figure>
                 <img src={sun} height={180} alt="sun"/>
                     <figcaption>
@@ -82,7 +87,7 @@ function Main2(){
                     </figcaption>
                 </figure>
             </button>
-            <button style={{backgroundColor:'lightblue'}}>
+            <button id="buttonMoon" style={{backgroundColor:'lightskyblue'}}>
                 <figure>
                 <img src={moon} height={180} alt="moon"/>
                     <figcaption>
