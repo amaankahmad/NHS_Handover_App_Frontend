5fa6610f8090996ca1eca50653e9bd9063b0b67d
kimfarhat <kf219@ic.ac.uk>
2021-12-25 21:21:11 +0200

    Page connections v3 (different ids in all the pages)

--- nhs_handover/src/NewTask/NewTask.js
+++ nhs_handover/src/NewTask/NewTask.js
@@ -21,42 +21,42 @@ function NewTask() {
             <div >
                 <h1 style={{color:'#21ACFA', margin: "15px 0",paddingLeft: "160px"}}>
                     NEW TASK:
-                    <img src={taskIcon} id = "taskIcon" height ={50} alt ="taskIcon"/>
+                    <img src={taskIcon} id = "taskIconN" height ={50} alt ="taskIcon"/>
                 </h1>
 
-                <div id="containerZero" className="container">
+                <div id="containerZeroN" className="container">
 
-                    <DateTime id={"DateTime"}/>
+                    <DateTime id={"DateTimeN"}/>
                 </div>
             </div>
-            <div id="containerOne" className="container">
+            <div id="containerOneN" className="container">
                 <div className="container" style={{background:'#ABDEFB', margin: "10px 0",paddingLeft: "50px" }} >
                     <div className="row" style={{margin: "10px 0",alignment:"center",paddingLeft: "0px"}}>
-                        <div className="col-2" id="patientName" style={{margin: "10px 0" }}> <input type="text" placeholder={"Patient Name"}/></div>
-                        <div className="col-2" id="MRNNumber" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Patient MRN Number"}/></div>
-                        <div className="col-2" id="NHSNumber" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Patient NHS Number"}/> </div>
-                        <div className="col-2" id="wardNumber" style={{margin: "10px 0" }}> <input type="text" placeholder={"Ward #"}/></div>
-                        <div className="col-4" id="bedNumber" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Bed #"}/></div>
+                        <div className="col-2" id="patientNameN" style={{margin: "10px 0" }}> <input type="text" placeholder={"Patient Name"}/></div>
+                        <div className="col-2" id="MRNNumberN" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Patient MRN Number"}/></div>
+                        <div className="col-2" id="NHSNumberN" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Patient NHS Number"}/> </div>
+                        <div className="col-2" id="wardNumberN" style={{margin: "10px 0" }}> <input type="text" placeholder={"Ward #"}/></div>
+                        <div className="col-4" id="bedNumberN" style={{ margin: "10px 0" }}> <input type="text" placeholder={"Bed #"}/></div>
                     </div>
 
 
                     <div className="row" style={{margin: "10px 0",paddingLeft: "15px"}}>
-                        <div className="col-4" id="DOB" style={{margin: "10px 0" ,paddingLeft: "0px"}}> <input type="text" placeholder={"DOB"}/></div>
+                        <div className="col-4" id="DOBN" style={{margin: "10px 0" ,paddingLeft: "0px"}}> <input type="text" placeholder={"DOB"}/></div>
                         <div className="col-4" style={{margin: "10px 0" ,paddingLeft: "0px",color:'black'}}>
                                 {"\t"} Sex  :  {"\t"}
-                            <input type="checkbox" id="Female" name="F"/>
+                            <input type="checkbox" id="FemaleN" name="F"/>
                                 <label htmlFor="Female">F {"\t"} </label>
                             {"\t"}
-                            <input type="checkbox" id="Male" name="M"/>
+                            <input type="checkbox" id="MaleN" name="M"/>
                                 <label htmlFor="Female">M {"\t"}</label>
                         </div>
 
                         <div className="col-4" style={{margin: "10px 0" ,paddingLeft: "0px",color:'black'}}>
                                 {"\t"} {"\t"} Priority  :  {"\t"}
-                            <input type="checkbox" style={{margin:"0px"}} id="High" name="High"/>
+                            <input type="checkbox" style={{margin:"0px"}} id="HighN" name="High"/>
                                 <label htmlFor="High" style={{paddingLeft:"0px"}}>High {"\t"}</label>
                             {"\t"}
-                            <input type="checkbox" style={{marginRight:"0px", paddingLeft:"5px"}} id="Low" name="Low"/>
+                            <input type="checkbox" style={{marginRight:"0px", paddingLeft:"5px"}} id="LowN" name="Low"/>
                                 <label htmlFor="Low" style={{paddingLeft:"0px"}}>Low  </label>
                         </div>
                     </div>
@@ -64,9 +64,9 @@ function NewTask() {
                 </div>
             </div>
 
-            <div id="containerTwo" className="container">
+            <div id="containerTwoN" className="container">
                 <div className="btn-group dropdown">
-                    <button type="button" id="taskType" className="btn btn-secondary btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button type="button" id="taskTypeN" className="btn btn-secondary btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Task Type
                     </button>
                     <div className="dropdown-menu">
@@ -77,29 +77,29 @@ function NewTask() {
                 </div>
             </div>
 
-            <div id="containerThree" className="container">
+            <div id="containerThreeN" className="container">
                 <div className="container" style={{background: '#DDEFFA', margin: "10px 0", alignment: 'left'}}>
                     <div className="row">
 
                         <div className="col-4" style={{margin: "10px 0" ,color:'black'}}>
                             {"\t"} Seniority  :  {"\t"}
-                            <input type="checkbox" id="Junior" name="Junior"/>
+                            <input type="checkbox" id="JuniorN" name="Junior"/>
                             <label htmlFor="Junior">Junior{"\t"} </label>
                             {"\t"}
-                            <input type="checkbox" id="SHO" name="SHO"/>
+                            <input type="checkbox" id="SHON" name="SHO"/>
                             <label htmlFor="SHO">SHO {"\t"}</label>
                             {"\t"}
-                            <input type="checkbox" id="Consultant" name="Consultant"/>
+                            <input type="checkbox" id="ConsultantN" name="Consultant"/>
                             <label htmlFor="Consultant">Consultant {"\t"} </label>
                             {"\t"}
                         </div>
 
                         <div className="col-4" style={{margin: "10px 0" ,paddingLeft: "0px",color:'black'}}>
                             {"\t"} {"\t"} Covid Status  :  {"\t"}
-                            <input type="checkbox" style={{marginRight:"2px"}} id="Positive" name="Positive"/>
+                            <input type="checkbox" style={{marginRight:"2px"}} id="PositiveN" name="Positive"/>
                             <label htmlFor="Positive" style={{paddingLeft:"0px"}}>Positive {"\t"}</label>
                             {"\t"}
-                            <input type="checkbox" style={{marginRight:"0px", paddingLeft:"0px"}} id="Negative" name="Negative"/>
+                            <input type="checkbox" style={{marginRight:"0px", paddingLeft:"0px"}} id="NegativeN" name="Negative"/>
                             <label htmlFor="Negative" style={{paddingLeft:"2px"}}>Negative  </label>
                         </div>
                         <div className="col-4" style={{margin: "10px 0" ,paddingLeft: "00px"}}> <input type="text" placeholder={"Schedule"}/></div>
@@ -107,7 +107,7 @@ function NewTask() {
                 </div>
             </div>
 
-            <div id="containerFour" className="container">
+            <div id="containerFourN" className="container">
                 <div style={{color: 'black', background: '#DDEFFA'}}>
                     <h2>Additional Notes </h2>
                     -<p/>
@@ -116,7 +116,7 @@ function NewTask() {
                     -<p/>
                 </div>
             </div>
-            <button id="AddTask">
+            <button id="AddTaskN">
                     Add Task
             </button>
                 </>}/>
